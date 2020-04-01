e9cd67212bed0ebb8f137cc8de1df851e421df97
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-04-01 20:19:50 +0200

    testing deploying to zeit, updating URL

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "429f73652c971620618e";
+/******/ 	var hotCurrentHash = "7749b50e8ac798541327";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
